d3b236bb3e18a5d81dbd5086c0c8b90aba2aaff8
phattranduy <115158729+DuyPhatpeo@users.noreply.github.com>
2024-01-19 16:03:51 +0700

    Ví dụ đầu

---
+++ Hello/src/SayHello.java
@@ -0,0 +1,6 @@
+public class SayHello {
+    public static void main(String[] args) {
+        System.out.println("Xin chao. Toi la SV khoa 63 CNTT - NTU");
+        System.out.print("Tran Duy Phat");
+    }
+}
