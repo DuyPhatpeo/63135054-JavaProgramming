a8bab87c23a07f3eece77c38bf9552a33dbfb07d
phattranduy <115158729+DuyPhatpeo@users.noreply.github.com>
2024-01-23 16:54:17 +0700

    Lab 3 - bài 1

--- Lab/src/Lab3/Bai1.java
+++ Lab/src/Lab3/Bai1.java
@@ -1,5 +1,32 @@
 package Lab3;
 
-public class Bai1 {
+import java.util.Scanner;
 
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập vào một số nguyên dương: ");
+        int N = scanner.nextInt();
+
+        boolean ok = true;
+
+        if (N <= 1) {
+            ok = false; // Không phải số nguyên tố nếu là số âm hoặc bằng 1
+        } else {
+            for (int i = 2; i < N; i++) {
+                if (N % i == 0) {
+                    ok = false;
+                    break;
+                }
+            }
+        }
+
+        if (ok) {
+            System.out.println(N + " là số nguyên tố.");
+        } else {
+            System.out.println(N + " không phải là số nguyên tố.");
+        }
+    }
 }
+
