1662e53740bdeaf0712da46bc271a431038ed994
phattranduy <115158729+DuyPhatpeo@users.noreply.github.com>
2024-01-24 17:15:48 +0700

    Lab 4 - bài 1

--- Lab4/src/Bai1/SanPham.java
+++ Lab4/src/Bai1/SanPham.java
@@ -1,5 +1,39 @@
 package Bai1;
+import java.util.Scanner;
 
 public class SanPham {
+    private String tenSp;
+    private double donGia, giamGia;
 
+    public SanPham(String tenSp, double donGia, double giamGia){
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+
+    public double getThueNhapKhau(){
+        return 0.1*donGia;
+    }
+
+    public void xuat(){
+        System.out.println("Tên sản phẩm: " + tenSp);
+        System.out.println("Đơn giá: " + donGia);
+        System.out.println("Giảm giá: " + giamGia);
+        System.out.println("Thuế nhập khẩu: " + getThueNhapKhau());
+    }
+
+    public  void nhap(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên sản phẩm: ");
+        tenSp = scanner.nextLine();
+        System.out.print("Nhập đơn giá: ");
+        donGia = scanner.nextDouble();
+        System.out.print("Nhập giảm giá: ");
+        giamGia = scanner.nextDouble();
+    }
+    
+    public static void main(String[] args) {
+        SanPham sanPham1 = new SanPham("LapTop", 1000.00, 100.00);
+        sanPham1.xuat();
+    }
 }
