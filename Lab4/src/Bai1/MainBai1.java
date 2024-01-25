d0f9a6986eb52a9230fae71098daf6c6a16353e0
phattranduy <115158729+DuyPhatpeo@users.noreply.github.com>
2024-01-25 10:21:31 +0700

    Lab 4 - bài 2

--- Lab4/src/Bai1/MainBai1.java
+++ Lab4/src/Bai1/MainBai1.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 public class MainBai1 {
     public static void main(String[] args) {
         SanPham sp1 = new SanPham("Điện thoại",25000.00,20000);
+        System.out.println("Sản phẩm thứ 1:");
         sp1.xuat();
         SanPham sp2 = new SanPham("laptop", 20000.00,19000.00);
-        sp1.xuat();
+        System.out.println("Sản phẩm thứ 2:");
+        sp2.xuat();
+        System.out.println("Sản phẩm thứ 3");
         SanPham sp3 = new SanPham();
         sp3.nhap();
         sp3.xuat();
+        System.out.println("Sản phẩm thứ 4");
+        SanPham sp4 = new SanPham();
+        sp4.nhap();
+        sp4.xuat();
     }
 }
