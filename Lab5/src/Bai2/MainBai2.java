4ab5aa3f96f4ea07abee07c17473d53da62e80a7
phattranduy <115158729+DuyPhatpeo@users.noreply.github.com>
2024-01-31 09:53:48 +0700

    Chỉnh sửa lại bài 2 lab 5

--- Lab5/src/Bai2/MainBai2.java
+++ Lab5/src/Bai2/MainBai2.java
@@ -38,12 +38,14 @@ public class MainBai2 {
     }
 
     private static int menu(Scanner scanner) {
-        System.out.println("1. Nhập danh sách họ và tên");
-        System.out.println("2. Xuất danh sách vừa nhập");
-        System.out.println("3. Xuất danh sách ngẫu nhiên");
-        System.out.println("4. Sắp xếp giảm dần và xuất danh sách");
-        System.out.println("5. Tìm và xóa họ tên");
-        System.out.println("6. Kết thúc");
+        System.out.println("+---------------------------------------+");
+        System.out.println("| 1. Nhập danh sách họ và tên           |");
+        System.out.println("| 2. Xuất danh sách vừa nhập            |");
+        System.out.println("| 3. Xuất danh sách ngẫu nhiên          |");
+        System.out.println("| 4. Sắp xếp giảm dần và xuất danh sách |");
+        System.out.println("| 5. Tìm và xóa họ tên                  |");
+        System.out.println("| 6. Kết thúc                           |");
+        System.out.println("+---------------------------------------+");
         System.out.print("Chọn chức năng (1-6): ");
         return scanner.nextInt();
     }
