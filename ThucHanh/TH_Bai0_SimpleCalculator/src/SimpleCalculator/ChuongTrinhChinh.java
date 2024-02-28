4f2fe2df7b0b1ade719f251eb077fc34a0c96772
Tran Duy Phat <115158729+DuyPhatpeo@users.noreply.github.com>
2024-02-28 13:13:43 +0700

    TH Bai 0

---
+++ ThucHanh/TH_Bai0_SimpleCalculator/src/SimpleCalculator/ChuongTrinhChinh.java
@@ -0,0 +1,8 @@
+package SimpleCalculator;
+
+public class ChuongTrinhChinh {
+	public static void main(String[] args) {
+		// Tạo màn hình GUI tính toán
+		new ManHinhTinhToan().setVisible(true);
+	}
+}
